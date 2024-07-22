3458e24d943645908dde79d2f20215b650e2f9ca
Lukáš Ondrejka <lukas.ondrejka1@gmail.com>
2024-07-22 20:41:05 +0200

    Add eleventy

---
+++ .eleventy.js
@@ -0,0 +1,45 @@
+const eleventySass = require('eleventy-sass');
+const htmlMinifier = require('html-minifier');
+
+const inputDir = 'src';
+const outputDir = 'dist';
+
+module.exports = function(eleventyConfig) {
+  // Copy static files
+  eleventyConfig.addPassthroughCopy({
+    [`${inputDir}/static`]: '/',
+  });
+
+  // Compile scss files
+  eleventyConfig.addPlugin(eleventySass, {
+    compileOptions: {
+      permalink: function(contents, inputPath) {
+        return (data) => data.page.filePathStem.replace(/^\/scss\//, '/css/') + '.css';
+      }
+    },
+    sass: {
+      style: 'compressed',
+      sourceMap: false,
+    },
+  });
+
+  // Minify HTML
+  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      return htmlMinifier.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+    } else {
+      return content;
+    }
+  });
+
+  return {
+    dir: {
+      input: inputDir,
+      output: outputDir,
+    },
+  };
+}
--- app.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    var canonicalLink = document.querySelector("link[rel='canonical']").href;
-    var currentLink = window.location.href;
-
-    if (canonicalLink != currentLink) {
-        window.location.href = canonicalLink;
-    }
-}
